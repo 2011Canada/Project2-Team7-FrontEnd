0043addbe61fdf22bd28bfbaa21c59a92771e380
domanic-yalda <domanic.yalda@ryerson.ca>
2021-01-08 12:30:49 -0500

    register template

--- mixology-101/src/App.tsx
+++ mixology-101/src/App.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import {RegisterForm} from './components/RegisterForms/Register'
 import './App.css';
+
+
+
+
 
 function App() {
   return (
     <div className="App">
       <h1>Mixology-101</h1>
+       <Router>
+        <div id = "root">
+        </div>  
+          <header className="App-header">
+            <Switch>
+              <Route path = "/register" component = {RegisterForm}/>
+            </Switch>
+          </header> 
+      </Router> 
+
     </div>
   );
 }
