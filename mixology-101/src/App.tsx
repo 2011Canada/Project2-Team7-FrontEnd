3622fe847f3b3f42a1c9b9b3b9697e68c3f3d9f0
Rous Epistola <rousbepistola@yahoo.com>
2021-01-07 21:37:49 -0500

    Initial implementation of homePage and it's components. Added utils folder for axios

--- mixology-101/src/App.tsx
+++ mixology-101/src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MainPage from './components/MainPage';
 
 function App() {
   return (
     <div className="App">
-      <h1>Mixology-101</h1>
+      <MainPage />
     </div>
   );
 }
+
 
 export default App;
---
+++ mixology-101/src/components/MainPageComponents/MainBody.tsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+const bodyStyle = {
+  height: '70vh',
+  backgroundColor: "lightgrey"
+}
+
+const MainBody = ()=>{
+
+    return(
+        <div style={bodyStyle}>
+            {/* There will be a bubble profile on left */}
+            {/* cards will be presented by card grid for drinks */}
+            {/* This is body this will be in cards */}
+            <h1>Main Page</h1>
+        </div>
+    )
+
+}
+
+export default MainBody
