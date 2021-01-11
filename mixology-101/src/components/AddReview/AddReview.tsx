b57405282a681253bc64c12dcfe11d191202fdf8
syed azib <syed.azib@hotmail.com>
2021-01-11 12:07:56 -0500

    updated review form

---
+++ mixology-101/src/components/AddReview/AddReview.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AddReviewHeader from "../AddReviewHeader/AddReviewHeader";
+
+function AddReview() {
+    return (
+        <div className="AddReviewHeader">
+            <AddReviewHeader />
+        </div>
+    )
+}
+
+export default AddReview;
