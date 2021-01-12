70fd119dd98532a5f7194dd7d2c50e128022dec0
Rous Epistola <rousbepistola@yahoo.com>
2021-01-11 21:03:17 -0500

    working get all drinks

--- mixology-101/src/components/MainPageComponents/ProfileBar/ProfileBar.tsx
+++ mixology-101/src/components/MainPageComponents/ProfileBar/ProfileBar.tsx
@@ -14,10 +14,27 @@ const imageStyle = {
     margin: "0.8rem"
 }
 
+const SearchButtonStyling = {
+    width: "70px",
+    height: "70px",
+    padding: "10px 16px",
+    borderRadius: "35px",
+    fontSize: "24px",
+    lineHeight: "1.33",
+    position: "absolute" as "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "Translate(-50%, -50%)",
+    border: "1px solid #020202",
+    // "-webkit-box-shadow": "5px 5px 15px 5px #fcfcfc",
+    "box-shadow": "2.5px 2.5px 15px 2.5px #e7caca"
+}
 
-const ProfileBar = () =>{
 
-    
+const ProfileBar = (props:any) =>{
+
+
+
     return(
         <div style={barStyle} className="col-12">
             <div className="row">
@@ -26,17 +43,40 @@ const ProfileBar = () =>{
                     <h5>Azib RousDomJin</h5>
                     <p>username : jin | id: 54</p>
                 </div>
-                <div style={{}} className="col-12 col-sm-4">
-                    <form>
-                        <div className="form-group">
-                            <label htmlFor="exampleFormControlSelect1">Search by </label>
-                            <select className="form-control" id="exampleFormControlSelect1">
-                            <option>Favorite</option>
-                            <option>Strength</option>
-                            <option>Review</option>
-                            <option>Ingredient</option>
-                            </select>
-                        </div>
+
+                {/* SEARCH PARAMS */}
+                <div style={{}} className="col-12 col-sm-2 ">
+                    <form onSubmit={props.call} style={{height:"100%"}}>
+                            <button  style={SearchButtonStyling} type="submit" className="btn btn-success btn-circle btn-xl">
+                                <i className="fa fa-cocktail"></i>
+                            </button>
+                            <p className="badge">All Drinks</p>
+                            
+                    </form>
+                </div>
+                <div style={{}} className="col-12 col-sm-2 ">
+                    <form style={{height:"100%"}}>
+                            <button style={SearchButtonStyling} type="button" className="btn btn-danger btn-circle btn-xl">
+                                <i className="fa fa-heart"></i>
+                            </button>
+                            <p className="badge">Favorites</p>
+                    </form>
+                </div>
+                <div style={{}} className="col-12 col-sm-2 ">
+                    <form style={{height:"100%"}}>
+                            <button style={SearchButtonStyling} type="button" className="btn btn-warning btn-circle btn-xl">
+                                <i className="fas fa-percentage"></i>
+                            </button>
+                            <p className="badge">Percentage</p>
+                    </form>
+                </div>
+
+                <div style={{}} className="col-12 col-sm-2 ">
+                    <form style={{height:"100%"}}>
+                            <button style={SearchButtonStyling} type="button" className="btn btn-info btn-circle btn-xl">
+                                <i className="fas fa-mortar-pestle"></i>
+                            </button>
+                            <p className="badge">Search By Ingridient</p>
                     </form>
                 </div>
 
